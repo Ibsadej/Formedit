862ba70d7e8d4d913647d160bad9c552c53a8d7c
Ibsa dejuma <ibsadejuma2@gmail.com>
2024-01-05 18:46:23 +0300

    Made some changes to the responsive design

--- JS/script.js
+++ JS/script.js
@@ -9,18 +9,15 @@ icon.forEach((icon)=>{
     }
 })
 
-const menu = document.querySelector(".menu");
-const menu1 = document.querySelector(".menu1");
+const menu = document.querySelector(".menu-icon-container");
 const responsivness = document.querySelector(".responsive");
 const blure = document.querySelector(".side-bar-blur")
 
 menu.onclick = ()=>{
     responsivness.classList.toggle("active");
     blure.classList.toggle("blured")
-}
-menu1.onclick = ()=>{
-    responsivness.classList.toggle("active");
-    blure.classList.toggle("blured")
+    menu.classList.toggle("active")
+
 }
 
 const caretBtn = document.querySelectorAll(".caret-btn");
